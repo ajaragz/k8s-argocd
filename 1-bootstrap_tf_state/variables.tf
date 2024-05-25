3daed432ab941132e88d90e1f64ec5edfd928ecb
Angel Jara <Angel.Jara@ingrammicro.com>
2024-05-26 00:30:09 +0200

    Add region variable

--- 1-bootstrap_tf_state/variables.tf
+++ 1-bootstrap_tf_state/variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "bucket_name" {
   description = "Name of the bucket that stores the terraform state"
   type        = string
--- 2-eks/variables.tf
+++ 2-eks/variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
