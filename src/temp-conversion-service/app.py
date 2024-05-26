3485d4f5393b91987238ac3d4e9ed575508800a6
Angel Jara <Angel.Jara@ingrammicro.com>
2024-05-26 11:33:02 +0200

    Add services

---
+++ src/temp-conversion-service/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Temperature Conversion Service is running"
+
+@app.route('/convert', methods=['GET'])
+def convert_temperature():
+    try:
+        temp = request.args.get('temp', type=float)
+        scale = request.args.get('scale', type=str)
+
+        if scale == 'C':
+            converted_temp = (temp * 9/5) + 32  # Convert Celsius to Fahrenheit
+        elif scale == 'F':
+            converted_temp = (temp - 32) * 5/9  # Convert Fahrenheit to Celsius
+        else:
+            app.logger.error("Invalid scale provided. Use 'C' for Celsius and 'F' for Fahrenheit.")
+            return jsonify({"error": "Invalid scale. Use 'C' for Celsius and 'F' for Fahrenheit."}), 400
+
+        return jsonify({"converted_temp": converted_temp})
+    except Exception as e:
+        app.logger.error(f"Error converting temperature: {e}")
+        return jsonify({"error": "Unable to convert temperature"}), 500
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=8082)
+    except Exception as e:
+        app.logger.error(f"Error starting the application: {e}")
---
+++ src/weather-service/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Sample static weather data
+weather_data = {
+    "city": "Sevilla",
+    "temperature": "20",
+    "condition": "Sunny",
+    "humidity": "50%"
+}
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Weather Service is running"
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    try:
+        return jsonify(weather_data)
+    except Exception as e:
+        app.logger.error(f"Error fetching weather data: {e}")
+        return jsonify({"error": "Unable to fetch weather data"}), 500
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=8081)
+    except Exception as e:
+        app.logger.error(f"Error starting the application: {e}")
